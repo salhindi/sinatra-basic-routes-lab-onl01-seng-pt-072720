d1f911ad71fbf0500b79e2f5b229beb2f4fd017f
Sara A <calisaraalhindi@gmail.com>
2020-10-20 00:10:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,4 +8,12 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Sara"
   end
+  
+  get '/hometown' do
+    "My hometown is Stockton"
+  end
+  
+  get '/favorite--song' do
+    "My favorite song is 3 Daqat"
+  end
 end
